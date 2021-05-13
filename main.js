cecf50010d0dbc2375058864758778963fa448fb
Joon Woo Park <canejune@gmail.com>
2021-05-12 23:49:38 -0700

    add file read

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, dialog } = require('electron');
 const path = require('path');
+const fs = require('fs')
+const es = require('event-stream')
 
-require('electron-reload')(__dirname);
+// require('electron-reload')(__dirname);
 
 let mainWindow;
 
@@ -17,6 +19,39 @@ function createWindow() {
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
+
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        { label: 'Open Log File',
+          click: () => {
+            dialog.showOpenDialog(
+                {
+                  properties: ['openFile']
+                }).then(result => {
+                    if (!result.canceled) {
+                        console.log(result.filePaths)
+                        startTimeMs = -1;
+                        mainWindow.webContents.send('rexTable', rexTable);
+                        readFile(result.filePaths[0])
+                    }
+                }).catch(err => {
+                  console.log(err)
+                })
+        }},
+        { role: 'quit' }
+      ]
+    },
+     { role: 'editMenu' },
+     { role: 'viewMenu' },
+     { role: 'windowMenu' },
+     { role: 'help' }
+    ];
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
 };
 
 app.whenReady().then(() => {
@@ -34,3 +69,94 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+
+
+
+
+var totalLine = 0;
+
+rexTable = [
+  {
+    rex: /GlMeSrdAsicinit/,
+    option:'rex',
+    mode: 'show',
+    fgRGB: [0, 0, 0],
+    bgRGB: [255, 255, 255],
+  },
+  {
+    rex: /elapsed/,
+    option:'rex',
+    mode: 'show',
+    fgRGB: [255, 0, 0],
+    bgRGB: [255, 255, 255],
+  },
+  {
+    rex: /Error/,
+    option:'rex',
+    mode: 'show',
+    fgRGB: [0, 0, 0],
+    bgRGB: [255, 0, 0],
+  },
+  {
+    rex: /AddCrcRequest/,
+    option:'rex',
+    mode: 'show',
+    fgRGB: [0, 0, 255],
+    bgRGB: [255, 255, 255],
+  },
+];
+
+let parsedLines = [];
+let startTimeMs =  -1;
+
+function parseLine(line, lineNumber) {
+  const regexTime = /^\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}\.\d{3}/;
+  let found = [];
+  let parsedLine = null;
+
+  for (i in rexTable) {
+    r = line.search(rexTable[i].rex);
+    if (r >= 0) {
+      found.push(i);
+    }
+  }
+
+  if (found.length > 0) {
+    let timeMs = new Date(line.match(regexTime)[0]).getTime();
+    if (startTimeMs < 0) {
+      startTimeMs = timeMs;
+    }
+    //console.log(lineNumber, ' : ', found, ' : ', timeMs, ' : ', line);
+    parsedLine = {
+      lineNumber: lineNumber,
+      timeMs: timeMs - startTimeMs,
+      indexSearchTable: found,
+      line : line
+    };
+    parsedLines.push(parsedLine);
+  }
+  return parsedLine;
+}
+
+function readFile(filePath) {
+  let s = fs.createReadStream(filePath)
+  .pipe(es.split())
+  .pipe(
+    es.mapSync( line => {
+        totalLine++;
+        //console.log(line);
+        let pl = parseLine(line, totalLine);
+        if (pl) {
+          mainWindow.webContents.send('parsedLine', pl);
+        }
+    })
+    .on('error', err => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log('Read All');
+        console.log(totalLine);
+    })
+  )
+}
--- preload.js
+++ preload.js
@@ -20,15 +20,17 @@ window.onload = function () {
 
   myCanvas.addEventListener('mousemove', (e) => {
     if (e.buttons !== 0) {
-      posY = e.offsetY;
+      posY = e.offsetY < 20 ? 0 : e.offsetY - 20;
       drawBox(posY);      
+      updateContents(lines, posY, posY + 30)      ;
     } 
   });
 
   myCanvas.addEventListener('mousedown', (e) => {
     if (e.buttons !== 0) {
-      posY = e.offsetY;
-      drawBox(posY);      
+      posY = e.offsetY < 20 ? 0 : e.offsetY - 20;
+      drawBox(posY);
+      updateContents(lines, posY, posY + 30)      ;
     } 
   });
 
@@ -39,4 +41,65 @@ window.onresize = function () {
   myCanvas.height = window.innerHeight - 20;
 
   drawBox(posY);
+}
+
+
+const {ipcRenderer, TouchBarSlider} = require('electron');
+
+let rexTable = []
+let lines = []
+
+ipcRenderer.on('rexTable', (event, rt) => {
+  rexTable = rt;
+  console.log(rexTable);
+  lines = []
+})
+
+ipcRenderer.on('parsedLine', (event, line) => {
+    lines.push(line);
+    const searchResultsTable = document.getElementById('search_results_table');
+
+    var tr = document.createElement('tr');
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
+    fgRGB = rexTable[line.indexSearchTable[0]].fgRGB;
+    bgRGB = rexTable[line.indexSearchTable[0]].bgRGB;
+    td3.style.color = `rgb(${fgRGB[0]},${fgRGB[1]},${fgRGB[2]})`
+    td3.style.backgroundColor  = `rgb(${bgRGB[0]},${bgRGB[1]},${bgRGB[2]})`
+
+    td1.appendChild(document.createTextNode(`${line.lineNumber}`));
+    td2.appendChild(document.createTextNode(`${line.timeMs}`));
+    td3.appendChild(document.createTextNode(`${line.line}`));
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+
+    searchResultsTable.appendChild(tr);
+});
+
+function updateContents(lines, start, end) {
+  const searchResultsTable = document.getElementById('search_results_table');
+  searchResultsTable.innerHTML = ''
+  lines.slice(start, lines.length < end ? lines.length : end).forEach(line => {
+    var tr = document.createElement('tr');
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+    var td3 = document.createElement('td');
+    fgRGB = rexTable[line.indexSearchTable[0]].fgRGB;
+    bgRGB = rexTable[line.indexSearchTable[0]].bgRGB;
+    td3.style.color = `rgb(${fgRGB[0]},${fgRGB[1]},${fgRGB[2]})`
+    td3.style.backgroundColor  = `rgb(${bgRGB[0]},${bgRGB[1]},${bgRGB[2]})`
+
+    td1.appendChild(document.createTextNode(`${line.lineNumber}`));
+    td2.appendChild(document.createTextNode(`${line.timeMs}`));
+    td3.appendChild(document.createTextNode(`${line.line}`));
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+
+    searchResultsTable.appendChild(tr);
+  });
 }
